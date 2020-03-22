343f71d78eecb483eca72e82d89ed04c59aaea46
techflowing <techflowing@gmail.com>
2020-03-22 21:31:47 +0800

    添加laravel-debugbar

--- app/Model/navigation/Category.php
+++ app/Model/navigation/Category.php
@@ -5,10 +5,33 @@ namespace App\Model\navigation;
 use Encore\Admin\Traits\AdminBuilder;
 use Encore\Admin\Traits\ModelTree;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * 定义网站分类Model
+ * Class Category
+ * @package App\Model\navigation
+ */
 class Category extends Model
 {
     use ModelTree, AdminBuilder;
 
     protected $table = 'navigation_categories';
+
+    /**
+     * 定义有多个子Category
+     * @return HasMany
+     */
+    public function children()
+    {
+        return $this->hasMany(static::class, 'parent_id');
+    }
+
+    /**
+     * 定义一对多关系，一个分类下有多个地址索引
+     */
+    public function sites()
+    {
+        return $this->hasMany(Site::class);
+    }
 }
--- app/Model/navigation/Site.php
+++ app/Model/navigation/Site.php
@@ -4,12 +4,17 @@ namespace App\Model\navigation;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 定义具体网址信息
+ * Class Site
+ * @package App\Model\navigation
+ */
 class Site extends Model
 {
     protected $table = 'navigation_sites';
 
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'category_id');
     }
 }
