fa6f481f3002cebe684383625facf4bc710f1146
techflowing <techflowing@gmail.com>
2020-05-31 22:59:57 +0800

    修改首页样式

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Admin\HomeNavMenu;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('welcome.index');
+        $navMenu = HomeNavMenu::all()
+            ->sortBy('order');
+
+        return view('welcome.index')
+            ->with('homeMenu', $navMenu);
     }
 }
--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -3,14 +3,33 @@
 @include('welcome.header')
 
 @section('content')
-    <div class="circle-container">
-        <div class="outer-circle" onclick="window.open('/blog','_self')">
-            <div class="inner-circle">
+    <div class="welcome-container">
+        <div class="welcome-center-container">
+            <div class="outer-circle" onclick="window.open('/blog','_self')">
+                <div class="inner-circle">
+                </div>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+
+            <div class="welcome-profile-container">
+                @php
+                    $userSlogin = config('user_slogin','Coding For Fun，代码改变世界');
+                @endphp
+                <p class="user-profile-slogin">{{$userSlogin}}</p>
+            </div>
+
+            @isset($homeMenu)
+                <div class="welcome-nav-menu-container">
+                    @foreach($homeMenu as $menu)
+                        <a class="welcome-nav-menu" href="/{{$menu->path}}">{{$menu->name}}</a>
+                    @endforeach
+                </div>
+            @endisset
         </div>
+
     </div>
+
 @endsection
