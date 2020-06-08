c5545563901b45cd7b4d6ab9fe9ff91b2476cf91
techflowing <techflowing@gmail.com>
2020-06-08 23:05:31 +0800

    添加打点类

---
+++ app/Model/Statistic/PV.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model\Statistic;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * PV 统计表
+ * @package App\Model\Statistic
+ */
+class PV extends Model
+{
+
+}
---
+++ app/Model/Statistic/UV.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Model\Statistic;
+
+
+class UV
+{
+
+}
